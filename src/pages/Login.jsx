19c073656e141386ce97a6490311bd854e13b20d
oriane-houssin <117643200+oriane-houssin@users.noreply.github.com>
2025-05-19 17:14:32 -0400

    redirection après login + stockage du token en localstorage

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 import API from '../services/api';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
@@ -8,12 +9,16 @@ import Box from '@mui/material/Box';
 export default function Login() {
     const [form, setForm] = useState({email: "", password: ""});
     const [error, setError] = useState('');
+    const navigate = useNavigate();
 
     const handleChange = e => setForm({...form, [e.target.id]: e.target.value });
     const handleSubmit = async e => {
         e.preventDefault();
         try{
             const res = await API.post('/auth/login', form);
+            localStorage.setItem('token', res.data.token);
+            localStorage.setItem('user', JSON.stringify(res.data.user));
+            navigate('/');
         } catch (err) {
             setError('email ou mot de passe incorrect');
         }
