b6dc26494ebaa83279a252b31baf5a0b2826bce4
oriane-houssin <117643200+oriane-houssin@users.noreply.github.com>
2025-06-02 17:27:52 +0200

    Rating restaurant

--- src/composants/RestaurantMeta.jsx
+++ src/composants/RestaurantMeta.jsx
@@ -6,32 +6,41 @@ import {useState, useEffect} from 'react';
 import API from '../services/api.js';
 
 export default function RestaurantMeta({average, cuisine, vegan, vegetarian, wheelchair, delivery, takeaway, r_id, user}) {
-    const [rating, setRating] = useState(0);
+    const [userRating, setUserRating] = useState(0);
     const [liked, setLiked] = useState(false);
 
     useEffect(() => {
-        const checkIsFavorite = async () => {
+        const fetchData = async () => {
             if (user && r_id) {
                 try {
+                    //VERIF FAVORIS
                     const response = await API.get(`favorites/${r_id}`);
                     if (response.data.favorite) {
                         setLiked(true);
                     } else {
                         setLiked(false)
                     }
+                    //VERIF RATING
+                    const rating = await API.get(`ratings/${r_id}`);
+                    if (rating.data.userRating !== null) {
+                        setUserRating(rating.data.userRating);
+                    } else {
+                        setUserRating(0)
+                    } console.log(rating.data.userRating);
                 } catch (err) {
                     console.log(err);
                     setLiked(false);
+                    setUserRating(0);
                 }
                 console.log(liked);
             }
         };
-        checkIsFavorite();
+        fetchData();
     }, [r_id, user]);
 
+    //LIKER UN RESTO
     const toggleLike = async () => {
         if(!user) return alert("Connexion requise");
-
         try {
             if(!liked) {
                 await API.post('/favorites', {restaurant_id: r_id});
@@ -41,6 +50,28 @@ export default function RestaurantMeta({average, cuisine, vegan, vegetarian, whe
                 await API.delete(`favorites/${r_id}`);
                 console.log('Supprimé des favoris');
                 setLiked(false);
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    //NOTER UN RESTO
+    const handleRatingClick = async (newRating) => {
+        if (!user) {
+            return alert("Connexion requise");
+        }
+        try {
+            const response = await API.post('/ratings', {
+                restaurant_id: r_id,
+                rating: newRating
+            });
+            if (response.data.success) {
+                setUserRating(newRating);
+                console.log(newRating);
+            } else {
+                console.error(error);
+                alert("Une erreur est survenue");
             }
         } catch (err) {
             console.error(err);
@@ -61,12 +92,12 @@ export default function RestaurantMeta({average, cuisine, vegan, vegetarian, whe
 
             {/*USER'S RATING*/}
             <div className="flex flex-col items-center gap-2 ">
-                <p className="text-sm mb-2">Rate</p>
+                <p className="text-sm mb-2">Votre Note</p>
                 <div className="flex justify-center gap-1">
                     {[1,2,3,4,5].map((i) => (
-                        <StarIcon key={i} className={`cursor-pointer ${i <= rating ? 'text-green' : 'text-gray-600'}`}
-                        fill={i <= rating ? 'currentColor' : 'none'}
-                        onClick={() => setRating(i)}/>
+                        <StarIcon key={i} className={`cursor-pointer transition-colors ${i <= userRating ? 'text-green' : 'text-gray-600'}`}
+                        fill={i <= userRating ? 'currentColor' : 'none'}
+                        onClick={() => handleRatingClick(i)}/>
                     ))}
                 </div>
             </div>
