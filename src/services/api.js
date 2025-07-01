e60a05fb2d63b4b4898fa006cd853527e0af08ac
oriane-houssin <117643200+oriane-houssin@users.noreply.github.com>
2025-07-01 19:09:01 +0200

    modif env pour co back

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api'; // Fallback pour la sécurité
+const baseURL = process.env.VITE_API_BASE_URL || 'http://localhost:5000/api'; // Fallback pour la sécurité
 const API = axios.create({
     baseURL: baseURL
 });
